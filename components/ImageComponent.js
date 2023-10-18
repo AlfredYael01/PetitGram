ea7ff40f252882c1aa4cdab1d2c7d0407e7f6b58
Yael <yael.alfred@etu.iut-tlse3.fr>
2023-10-18 14:07:23 +0200

    Feat user profile screen gallery add border on photo Yael Alfred

--- components/ImageComponent.js
+++ components/ImageComponent.js
@@ -1,7 +1,11 @@
-import {Dimensions, FlatList, Image} from 'react-native';
+import {Dimensions, FlatList, Image, TouchableOpacity} from 'react-native';
+import 'react-native-gesture-handler';
+
 
 const ImageCompenent = ({ ImagesArray }) => (
-    <Image source={ImagesArray} style={{ width: Dimensions.get("window").width*0.35, height: Dimensions.get("window").height*0.1667}} />
+    <TouchableOpacity onPress={}>
+        <Image source={ImagesArray} style={{ borderColor: 'black', borderWidth:0.5, width: Dimensions.get("window").width*0.35, height: Dimensions.get("window").height*0.1667}} />
+    </TouchableOpacity>
 );
 
 export default ImageCompenent;
