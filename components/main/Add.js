b38c3e669625269ea2a749c07ec9c9f6bd7ae6f4
ismail <ismailalri@gmail.com>
2023-10-18 09:36:08 +0200

    feat : UI add, selection des photos

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,68 +1,233 @@
+import React, { useState, useEffect } from 'react';
+import { View, Image, FlatList, Button, StyleSheet, Text, TouchableOpacity, Dimensions } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
 import * as ImagePicker from 'expo-image-picker';
-import { useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
 
-export default function App() {
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [image, setImage] = useState(null);
+const { width } = Dimensions.get('window');
 
-  function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
-  }
+export default function AddScreen() {
+    const [type, setType] = useState(CameraType.back);
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [galleryImages, setGalleryImages] = useState([]);
+    const [selectedImages, setSelectedImages] = useState([]);
+    const [lastSelectedImage, setLastSelectedImage] = useState(null);
+    const [permission, requestPermission] = Camera.useCameraPermissions();
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [1, 1],
-      quality: 1,
-    });
-
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
+    function toggleCameraType() {
+        setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
     }
-  };
 
+    const pickImage = async () => {
+        if (selectedImages.length < 10) {
+            let result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.All,
+                allowsEditing: true,
+                aspect: [1, 1],
+                quality: 1,
+            });
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.camera}>
-        <Camera  type={type} style={styles.camera} />
-      </View>
-      <View style={styles.image}>
-        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      </View>
-      <Button style={styles.button} onPress={toggleCameraType} title="Flip Camera" />
-      <Button style={styles.button} onPress={pickImage} title="Pick Image" />
-    </View>
-  );
+            if (!result.canceled) {
+                setSelectedImages([...selectedImages, result.assets[0].uri]);
+                setLastSelectedImage(result.assets[0].uri);
+            }
+        } else {
+            // Handle when the user tries to select more than 10 images
+            alert('You can select a maximum of 10 images.');
+        }
+    }
+
+    useEffect(() => {
+        // Load gallery images on component mount
+        (async () => {
+            const { status } = await MediaLibrary.requestPermissionsAsync();
+            if (status === 'granted') {
+                const assets = await MediaLibrary.getAssetsAsync({ mediaType: 'photo' });
+                setGalleryImages(assets);
+            }
+        })();
+    }, []);
+
+    const toggleImageSelection = (imageUri) => {
+        if (selectedImages.includes(imageUri)) {
+            setSelectedImages(selectedImages.filter(uri => uri !== imageUri));
+        } else {
+            if (selectedImages.length < 10) {
+                setSelectedImages([...selectedImages, imageUri]);
+                setLastSelectedImage(imageUri);
+            } else {
+                // Handle when the user tries to select more than 10 images
+                alert('You can select a maximum of 10 images.');
+            }
+        }
+    }
+
+    const removeSelectedImage = (imageUri) => {
+        setSelectedImages(selectedImages.filter(uri => uri !== imageUri));
+    }
+
+    return (
+        <View style={styles.container}>
+            {/* Header */}
+            <View style={styles.header}>
+                <Text style={styles.title}>New Post</Text>
+                <TouchableOpacity style={styles.nextButton} onPress={() => handleNext()}>
+                    <Text style={styles.nextButtonText}>Next</Text>
+                </TouchableOpacity>
+            </View>
+
+            {/* Selected Images */}
+            <View style={styles.selectedImagesContainer}>
+                {selectedImages.map((imageUri, index) => (
+                    <View key={index} style={styles.selectedImageItem}>
+                        <Image source={{ uri: imageUri }} style={styles.selectedImageThumbnail} />
+                        <TouchableOpacity style={styles.removeImageButton} onPress={() => removeSelectedImage(imageUri)}>
+                            <Text style={styles.removeImageText}>X</Text>
+                        </TouchableOpacity>
+                    </View>
+                ))}
+            </View>
+
+            {/* Image Preview */}
+            <View style={styles.imagePreview}>
+                {lastSelectedImage && (
+                    <Image source={{ uri: lastSelectedImage }} style={styles.selectedImageThumbnailBig} />
+                )}
+            </View>
+
+            {/* Image Picker and Camera */}
+            <View style={styles.imagePickerCamera}>
+                <TouchableOpacity style={styles.imagePickerButton} onPress={pickImage}>
+                    <Text style={styles.buttonText}>Pick Image</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.cameraButton} onPress={toggleCameraType}>
+                    <Text style={styles.buttonText}>Switch Camera</Text>
+                </TouchableOpacity>
+            </View>
+
+            {/* Gallery */}
+            <View style={styles.galleryContainer}>
+                <FlatList
+                    data={galleryImages.assets}
+                    renderItem={({ item }) => (
+                        <TouchableOpacity onPress={() => toggleImageSelection(item.uri)}>
+                            <Image
+                                source={{ uri: item.uri }}
+                                style={[
+                                    styles.galleryImage,
+                                    selectedImages.includes(item.uri) && styles.selectedGalleryImage,
+                                ]}
+                            />
+                        </TouchableOpacity>
+                    )}
+                    keyExtractor={item => item.id}
+                    numColumns={width < 768 ? 4 : 6}
+                />
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  camera: {
-    flex: 1,
-  },
-  buttonContainer: {
-    flex: 1,
-    backgroundColor: 'transparent',
-    flexDirection: 'row',
-    margin: 20,
-  },
-  button: {
-    flex: 0.1,
-    alignSelf: 'flex-end',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 18,
-    color: 'white',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#333',
+    },
+    header: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#333', // Dark background color for the header
+    },
+    title: {
+        fontSize: 24,
+        color: 'white',
+    },
+    nextButton: {
+        backgroundColor: 'gray',
+        padding: 10,
+        borderRadius: 5,
+    },
+    nextButtonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+    selectedImagesContainer: {
+        flexDirection: 'row',
+        padding: 10,
+        justifyContent: 'center',
+        backgroundColor: '#444', // A darker background for the selected images container
+    },
+    selectedImageItem: {
+        marginRight: 10,
+        position: 'relative',
+    },
+    removeImageButton: {
+        position: 'absolute',
+        top: 0,
+        right: 5,
+        backgroundColor: 'red',
+        borderRadius: 50,
+        width: 20,
+        height: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    removeImageText: {
+        color: 'white',
+    },
+    selectedImageThumbnail: {
+        width: 80,
+        height: 80,
+        maxWidth: '90%',
+        maxHeight: '90%',
+    },
+    selectedImageThumbnailBig: {
+        width: 300,
+        height: 300,
+        maxWidth: '90%',
+        maxHeight: '90%',
+    },
+    imagePreview: {
+        flex: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    imagePickerCamera: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        backgroundColor: '#444', // A darker background for the image picker and camera section
+    },
+    imagePickerButton: {
+        backgroundColor: 'gray',
+        padding: 10,
+        borderRadius: 5,
+    },
+    cameraButton: {
+        backgroundColor: 'gray',
+        padding: 10,
+        borderRadius: 5,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+    galleryContainer: {
+        flex: 4,
+        backgroundColor: '#444', // A darker background for the gallery section
+        padding: 5,
+    },
+    galleryImage: {
+        width: width < 768 ? (width / 4) - 8 : (width / 6) - 8,
+        height: width < 768 ? (width / 4) - 8 : (width / 6) - 8,
+        margin: 3,
+    },
+    selectedGalleryImage: {
+        opacity: 0.7,
+        borderColor: 'blue',
+        borderWidth: 2,
+    },
 });
