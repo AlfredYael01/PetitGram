0f2f37b8b5644d98e5db9ee4f4037e5320579a02
ismail <ismailalri@gmail.com>
2023-10-23 12:34:45 +0200

    modif jest

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,8 @@ const config = {
     transformIgnorePatterns: [
         'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.|@expo-google-fonts/.|react-navigation|@react-navigation/.|@unimodules/.|unimodules|sentry-expo|native-base|react-native-svg)',
       ],
+      testEnvironment: 'node',
       transform: {
-        '^.+\.[jt]sx?$': 'babel-jest',
       },
 };
 module.exports = config;
