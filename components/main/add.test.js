79af51c1c38679e6ac7dd113d4a276a9f3c612e2
ismail <ismailalri@gmail.com>
2023-10-24 16:07:06 +0200

    test : expo image picker

--- components/main/add.test.js
+++ components/main/add.test.js
@@ -1,6 +1,7 @@
 import 'react-native';
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import ImagePicker from 'expo-image-picker';
 import AddScreen from './Add';
 
 // Mock the modules that are not available in the test environment
@@ -11,10 +12,13 @@ jest.mock('expo-camera', () => ({
     front: 'front',
   },
 }));
-
 jest.mock('expo-image-picker', () => ({
   launchImageLibraryAsync: jest.fn(),
+  MediaTypeOptions: {
+    All: 'All', // Add MediaTypeOptions with 'All' to mock the MediaTypeOptions used in your component
+  },
 }));
+
 
 jest.mock('expo-media-library', () => ({
   requestPermissionsAsync: jest.fn(() => ({ status: 'granted' })),
@@ -47,6 +51,31 @@ test('renders AddScreen component without errors', () => {
 
   // Check if the component renders without errors
   expect(getByText('New Post')).toBeTruthy();
+  expect(getByText('Pick Image')).toBeTruthy();
   expect(queryByText('Post')).toBeTruthy();
+  expect(getByText('Switch Camera')).toBeTruthy();
 });
+
+test('expo image picker is called when the user clicks on the gallery button', () => {
+  
+  const { getByTestId } = render(<AddScreen navigation={jest.fn()} />);
+  // Mock the result to include a 'canceled' property
+  ImagePicker.launchImageLibraryAsync.mockResolvedValue({
+    canceled: false, // Set to true or false as needed
+    assets: [
+      {
+        uri: '../assets/Yo.jpg',
+      },
+    ],
+  });
+
+  // Click on the gallery button
+  const galleryButton = getByTestId('galleryButton');
+  fireEvent.press(galleryButton);
+  // Check if the pickImage function is called
+  expect(ImagePicker.launchImageLibraryAsync).toHaveBeenCalled();
+});
+
+
+
   
