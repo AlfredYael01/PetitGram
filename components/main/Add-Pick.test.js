eca645b21e77e0699348bddc53c1718de5ced2a3
Etudiant <anis.zizi@etu.iut-tlse3.fr>
2023-10-25 11:01:37 +0200

    modif test

---
+++ components/main/Add-Pick.test.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import UserProfileScreen from '../../assets/screens/userProfileScreen';
+import Add from './Add';
+import { MediaLibraryAssetInfoQueryOptions } from 'expo-media-library';
+// Mock the ImagePicker function
+jest.mock('expo-image-picker', () => {
+  return {
+    launchImageLibraryAsync: jest.fn(() => Promise.resolve({ uri: '../../assets/Yo.jpg', cancelled: false,
+
+    allowsEditing: true,
+    assets:[{uri:'../../assets/Yo.jpg'}],
+    aspect: [1, 1],
+    quality: 1, })),
+    MediaTypeOptions : jest.fn(),
+
+  }
+})
+
+jest.mock('expo-media-library',() => {
+  return{
+    requestPermissionsAsync: jest.fn(() => Promise.resolve({ status: 'granted' })),
+    getAssetsAsync: jest.fn(() => Promise.resolve({ mediaType: 'photo' })),
+  }
+})
+describe('Test pick an image and veryfy that it was picked', () => {
+  test('it should pick and display an image', async () => {
+    const { getByText, getByTestId } = render(<Add />);
+    
+    // Find and press the "Select Image" button
+    const selectImageButton = getByText('Pick Image');
+    fireEvent.press(selectImageButton);
+    
+    // Wait for the image to load
+    await waitFor(() => {
+      // Check if the selected image is displayed
+      const selectedImage = getByTestId("publication-image");
+      console.log(selectedImage);
+      expect(selectedImage).toBeTruthy();
+    });
+  });
+});
