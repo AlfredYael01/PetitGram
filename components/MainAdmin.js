11c25277204b1abbb4ddc4b90aa78a0f3ca048d1
Arguelles Gonzalez Luis <luis.arguelles-gonzalez@etu.iut-tlse3.fr>
2024-01-11 17:26:15 +0100

    Update MainAdmin component to display admin posts

--- components/MainAdmin.js
+++ components/MainAdmin.js
@@ -10,9 +10,10 @@ import {
   Image,
   Dimensions,
   TouchableOpacity,
+  FlatList
 } from "react-native";
 import { getFirestore, collection, getDocs, orderBy } from "firebase/firestore";
-import FeedPostItem from "./main/FeedPostItem";
+import FeedPostItemAdmin from "../components/main/FeedPostItemAdmin";
 import CommentsScreen from "./main/Comments";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchFeedPosts } from "../components/helper/posts";
@@ -33,16 +34,67 @@ const MainAdmin = () => {
     dispatch(fetchFeedPosts());
   }, []);
 
+
+  const PostScreen = React.memo(({ navigation }) => {
+    return (
+      <>
+        <View style={styles.header}>
+            <Text style={styles.headerTitle}>Admin</Text>
+            <TouchableOpacity style={styles.signoutButton} onPress={() => {
+              const auth = getAuth();
+              signOut(auth);
+            }}>
+              <Text style={{color: "white", fontWeight: "bold"}}>Signout</Text>
+            </TouchableOpacity>
+        </View>
+        <FlatList
+          data={posts}
+          renderItem={({ item, index }) =>  ( <FeedPostItemAdmin post={item} index={index} navigation={navigation} />)}
+          keyExtractor={(item) => item._id}
+        />
+      </>
+    );
+  });
+
+
   
   return (
     <View>
-      <Text>Admin</Text>
+      <PostScreen />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
- 
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10,
+    padding: 15,
+    borderBottomWidth: 0.5,
+    borderBottomColor: "black",
+  },
+  headerTitle: {
+    fontWeight: "bold",
+    fontSize: 20,
+    flex: 1,
+  },
+  headerUser: {
+    color: "gray",
+  },
+
+  signoutButton:{
+    height: Dimensions.get("window").height * 0.05,
+    width: Dimensions.get("window").width * 0.2,
+    backgroundColor: "black",
+    borderRadius: 12,
+    alignItems: "center",
+    justifyContent: "center",
+  }
   
 });
 
