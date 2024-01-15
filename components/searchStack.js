99b6459c692895faa2225e3692488e61ceb6bfcb
Arguelles Gonzalez Luis <luis.arguelles-gonzalez@etu.iut-tlse3.fr>
2024-01-15 09:03:26 +0100

    Refactor code to improve performance and readability

--- components/searchStack.js
+++ components/searchStack.js
@@ -2,7 +2,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
 import SearchScreen from './main/Search';
 import SearchUserProfileScreen from '../assets/screens/searchUserProfileScreen';
-import ViewPost from '../assets/screens/ViewPost';
+import ViewPostSearch from '../assets/screens/viewPostSearch';
 import UserProfileHeader from './userProfileHeader'
 
 const Stack = createStackNavigator();
@@ -13,7 +13,7 @@ const SearchStack = () => {
         <Stack.Navigator>
             <Stack.Screen name='searchScreen' component={SearchScreen} options={{headerShown: false}}/>
             <Stack.Screen name='searchUserProfileScreen' component={SearchUserProfileScreen} options={{headerShown: false}}/>
-            <Stack.Screen name='ViewPost' component={ViewPost} options={{headerTitle: "Publications"}}/>
+            <Stack.Screen name='ViewPostSearch' component={ViewPostSearch} options={{headerTitle: "Publications"}}/>
         </Stack.Navigator>
 
     )
